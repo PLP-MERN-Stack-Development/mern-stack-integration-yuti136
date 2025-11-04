73f6005d368e578a4b95088920b0534f098a7658
euticuskon19@gmail.com <euticuskon19@gmail.com>
2025-11-04 23:33:40 +0300

    Updated .env

--- mern-blog/client/src/services/api.js
+++ mern-blog/client/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 
 // 🔹 Backend API URL. Initially, the const API_BASE before deployment was "http://localhost:5000/api"
-const API_BASE = "https://mern-stack-integration-yuti136.onrender.com";
+const VILE_API_BASE_URL = "https://mern-stack-integration-yuti136.onrender.com";
 
 /**
  * 🔐 Fetch Clerk JWT token from the browser
