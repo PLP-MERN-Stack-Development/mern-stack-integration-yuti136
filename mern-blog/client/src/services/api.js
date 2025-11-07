ecd567b2fd0575f5188c24f56481ef36dd532123
euticuskon19@gmail.com <euticuskon19@gmail.com>
2025-11-07 13:52:20 +0300

    Updated

--- mern-blog/client/src/services/api.js
+++ mern-blog/client/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 
 // ✅ Use environment variable for backend URL
-const API_BASE = import.meta.env.VITE_API_BASE_URL;
+const API_BASE = import.meta.env.MONGODB_URI;
   
 
 /**
